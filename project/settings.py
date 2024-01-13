a7327f7362f8348fb3a7eb61d3b8da36e2fb5701
steven_savarin <srssdesing@gmail.com>
2024-01-14 00:28:44 +0100

    changes

--- project/settings.py
+++ project/settings.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,5 +126,5 @@ EMAIL = "django.core.mail.backends.smtp.EmailBacked"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "sawarinsteven@gmail.com"
-EMAIL_HOST_PASSWORD = "pyfd yleb wubu wnfr"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_UPASS")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASS")
